d6c2aa641d86d862fca4fba891c146998b5b78aa
tal21-meet <tal21@meet.mit.edu>
2019-07-28 04:13:53 -0400

    Stopping here

---
+++ fruit_soter.py
@@ -0,0 +1,9 @@
+fruit= input('whate is the kind of fruit?')
+if fruit=='apples':
+    print ("Apples in bin 1")
+elif fruit=='aoranges':
+    print("Oranges in bin 2")
+elif fruit=='apples':
+    print("Bananas in bin 3")
+else:
+    print('Error! I do not recognize this fruit!')
--- funturtle.py
+++ funturtle.py
@@ -1,16 +1,38 @@
 import turtle
-turtle.shape('square')
 def up():
-    turtle.forward(10)
+    turtle.forward(5)
 def down():
     turtle.backward(10)
 def left():
     turtle.right(90)
 def right():
     turtle.left(90)
+def num():
+	turtle.speed(0)
+	colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+	for x in range(360):
+	    turtle.pencolor(colors[x % 6])
+	    turtle.pencolor()
+	    turtle.begin_fill()
+	    turtle.forward(1)
+	    turtle.right(1)
+	    turtle.end_fill()
+def num1():
+    turtle.penup()
+    turtle.goto(0,0)
+    turtle.speed(0)
+    turtle.pencolor('red')
+    turtle.pendown
+    for x in range(360):
+        turtle.forward(x)
+        turtle.right(90)
+
 turtle.onkeypress(up, "w")
 turtle.onkeypress(down, "s")
 turtle.onkeypress(left, "d")
 turtle.onkeypress(right, "a")
+turtle.onkeypress(num, "q")
+turtle.onkeypress(num1, "1")
 turtle.listen()
-turtle.meinloop()
+
+turtle.mainloop()
